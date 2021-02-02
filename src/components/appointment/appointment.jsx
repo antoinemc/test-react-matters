cce7e0804e3a1928d164a347eca780f2afc4ad4b
Antoine MC <antoine.mc@hotmail.fr>
2021-02-02 01:06:26 +0100

    wip

--- src/components/appointment/appointment.jsx
+++ src/components/appointment/appointment.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './appointment.scss';
+import PropTypes from 'prop-types';
 
-const Appointment = () => {
+class Appointment extends React.Component {
 
-    render (
-            <div class="appointment">
-                <p>Merci ! <br/>
-                Rencontrons nous au Refuge de Paris pour finaliser l'adoption de votre chat !
-                </p>
+    render () {
+        if(!this.props.show) {
+            return null;
+          };
+
+          return (
+            <div className="container">
+                <div className="appointment">
+                    <p>Merci ! <br/>
+                    Rencontrons nous au Refuge de Paris pour finaliser l'adoption de votre chat !
+                    </p>
+                    <button onClick={this.props.onClose}>
+                        Close
+                    </button>
+                </div>
             </div>
-    )
-}
+            )
+          }
+    }
+
+Appointment.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    show: PropTypes.bool
+};
 
 export default Appointment;
--- src/components/cat/cat.jsx
+++ src/components/cat/cat.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
+import Appointment from '../appointment/appointment';
 import './cat.scss';
 
-const Cats = props => {
-    const { name, picturePath, gender, breed, birthdate, id} =
-    (props.location && props.location.state) || {};
-        return (
+class Cats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: false };
+        this.name =  this.props.name;
+      }
+
+      toggleModal = (state) => {
+        this.setState({
+          isOpen: !state.isOpen
+        });
+      }
+
+      render() {
+          if(!this.props.location && !this.props.location.state) {
+              return false;
+          }
+          return(
+            <div className="cat-container">
             <div className="cat">
-                <img src={picturePath} alt={name} className="cat__image"></img>
+                <img src={this.props.picturePath} alt={this.name} className="cat__image"></img>
                 <div className="cat__description">
                     <ul>
-                        <li>Nom : {name}</li>
-                        <li>Date de naissance : {birthdate} </li>
-                        <li>Genre : {gender}</li>
-                        <li>Race : {breed} </li>
+                        <li>Nom : {this.name}</li>
+                        <li>Date de naissance : {this.props.birthdate} </li>
+                        <li>Genre : {this.props.gender}</li>
+                        <li>Race : {this.props.breed} </li>
                     </ul>
-                <button className="cat__button">
+                <button className="cat__button" onClick={this.toggleModal}>
                     Prendre RDV
                 </button>
                 </div>
             </div>
-    
-            );
-      }
+                <div>
+                <Appointment show={this.state.isOpen} onClose={this.toggleModal}>
+                </Appointment>
+                </div>
+            </div>
+          )
+      } 
+} 
 
 export default Cats;
